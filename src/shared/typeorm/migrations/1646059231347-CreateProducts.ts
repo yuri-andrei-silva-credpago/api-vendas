857c2f49096d74d5f177cbe4858f7b0e6cd626ca
yuri <yuri.silva@credpago.com>
2022-02-28 11:44:17 -0300

    Criacao banco + migrations

---
+++ src/shared/typeorm/migrations/1646059231347-CreateProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateProducts1646059231347 implements MigrationInterface {
+
+    public async up(queryRunner:  QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
