e6f8789b3f6a6f0187cb240cd7d269c3d8adecb9
yuri <yuri.silva@credpago.com>
2022-03-03 11:19:05 -0300

    mail template handlebars teste

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -20,13 +20,23 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.')
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
 
     // console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        template: `Olá {{ name }}: {{ token }}`,
+        variables: {
+          name: user.name,
+          token,
+        }
+      }
     })
   }
 }
