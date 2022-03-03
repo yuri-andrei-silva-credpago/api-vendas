3c310628adced1503d4651d35219436e6e56497f
yuri <yuri.silva@credpago.com>
2022-03-03 15:11:07 -0300

    repository customers

--- src/modules/users/controllers/UserAvatarController.ts
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -4,7 +4,6 @@ export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
     const updateAvatar = new UpdateUserAvatarService();
 
-
     const user = updateAvatar.execute({
       user_id: request.user.id,
       avatarFilename: request.file.filename,
