e5af5849aa8e39804134df322181dcfccf28f59a
yuri <yuri.silva@credpago.com>
2022-03-03 16:00:06 -0300

    delete customer

---
+++ src/modules/customers/services/DeleteCustomerService.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import Customer from "../typeorm/entities/Customer";
+import CustomersRepository from "../typeorm/repositories/CustomersRepository";
+
+interface IRequest {
+  customer_id: string;
+}
+
+class DeleteCustomerService {
+  public async execute({ customer_id }: IRequest): Promise<void> {
+
+    const customersRepository = getCustomRepository(CustomersRepository);
+
+    const customer = await customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    await customersRepository.remove(customer);
+  }
+}
+
+export default DeleteCustomerService;
