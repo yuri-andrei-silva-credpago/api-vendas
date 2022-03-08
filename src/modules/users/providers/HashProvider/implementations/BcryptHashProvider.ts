dddd4358d1c1ee761c87127e8cd697d57508d75e
yuri <yuri.silva@credpago.com>
2022-03-08 14:05:26 -0300

    testes CreateSessionService

--- src/modules/users/providers/HashProvider/implementations/BcryptHashProvider.ts
+++ src/modules/users/providers/HashProvider/implementations/BcryptHashProvider.ts
@@ -5,8 +5,9 @@ class BcryptHashProvider implements IHashProvider {
   public async generateHash(payload: string): Promise<string> {
     return hash(payload, 8);
   }
+
   public async compareHash(payload: string, hashed: string): Promise<boolean> {
-    return compare(payload, hashed)
+    return compare(payload, hashed);
   }
 }
 
