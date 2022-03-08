f995af79aadde495afee809ad05fafaab5f2081c
yuri <yuri.silva@credpago.com>
2022-03-08 11:37:33 -0300

    refatorando servico CreateSessionService

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,16 +1,18 @@
 import { inject, injectable } from 'tsyringe';
 import AppError from "@shared/errors/AppError";
-import { compare, hash } from "bcryptjs";
 import { sign, Secret } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import { ICreateSession } from '../domain/models/ICreateSession';
 import { IUserAuthenticated } from '../domain/models/IUserAuthenticated';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 @injectable()
 class CreateSessionsService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
   ) {}
 
   public async execute({
@@ -23,7 +25,7 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination.', 401)
     }
 
-    const passwordConfirmed = await compare(password, user.password);
+    const passwordConfirmed = await this.hashProvider.compareHash(password, user.password);
 
     if (!passwordConfirmed) {
       throw new AppError('Incorrect email/password combination.', 401)
